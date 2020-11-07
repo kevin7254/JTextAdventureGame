005e72a2feb17bc1a95462689b9cb42650f22be5
kevinnilsson <kevin7254@gmail.com>
2020-11-07 02:18:15 +0100

    Starting on Game class

--- src/main/java/com/jtextadventure/game/Game.java
+++ src/main/java/com/jtextadventure/game/Game.java
@@ -1,4 +1,25 @@
 package com.jtextadventure.game;
 
+import com.jtextadventure.game.entities.Player;
+
+import java.util.Scanner;
+
 public class Game {
+
+    Player player;
+    Scanner sc;
+
+    public Game(Player player, String playerName) {
+
+        this.player = player;
+        this.sc = new Scanner(System.in);
+    }
+
+
+
+
+    public void newGameInit(Player player) {
+
+        player.setName(sc.nextLine());
+    }
 }
---
+++ src/main/java/com/jtextadventure/game/entities/Player.java
@@ -0,0 +1,7 @@
+package com.jtextadventure.game.entities;
+
+public class Player {
+
+    public void setName(String nextLine) {
+    }
+}
